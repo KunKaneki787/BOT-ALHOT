bd87e255979a2b03ea56cb05fb79b7da06a1df58
KunKaneki787 <140500647+KunKaneki787@users.noreply.github.com>
2023-11-01 13:56:45 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,16 +22,16 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
+global.ownernomer = "00249960928238" //ur number
+global.premium = ['00249960928238'] //ur premium number
+global.botname = '❤️ᗴᒪ-HOT ᗷOT❤️' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣الهوت عمك وعم اولادك⍣" //ur sticker watermark packname
+global.author = "⍣0960928238⍣" //ur sticker watermark author
+global.wm = "❤️ᗴᒪ-HOT ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
